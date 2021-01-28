57c90c1d4eb819220fbf307188008119b3606235
Myagmarsuren <miner_miga@yahoo.com>
2021-01-28 22:37:26 +0800

    Nemeh bolon suuliin element avhiig nemlee.

--- F.CS311-Lab1/src/test/ArrayQueue.java
+++ F.CS311-Lab1/src/test/ArrayQueue.java
@@ -20,6 +20,39 @@ public class ArrayQueue implements Queue {
 		this(10);
 	}
 	
+	public Object getRearElement()
+	   {
+	      if (isEmpty())
+	         return null;
+	      else
+	         return queue[rear];
+	   }
+	
+	public void put(Object theElement)
+	   {
+	      if ((rear + 1) % queue.length == front)
+	      {
+	         Object [] newQueue = new Object [2 * queue.length];
+	         int start = (front + 1) % queue.length;
+	         if (start < 2)
+	            System.arraycopy(queue, start, newQueue, 0,
+	                             queue.length - 1);
+	         else
+	         {
+	            System.arraycopy(queue, start, newQueue, 0,
+	                             queue.length - start);
+	            System.arraycopy(queue, 0, newQueue,
+	                             queue.length - start, rear + 1);
+	         }
+	         front = newQueue.length - 1;
+	         rear = queue.length - 2; 
+	         queue = newQueue;
+	      }
+
+	      rear = (rear + 1) % queue.length;
+	      queue[rear] = theElement;
+	   }
+	
 	public Object remove()
 	   {
 	      if (isEmpty())
