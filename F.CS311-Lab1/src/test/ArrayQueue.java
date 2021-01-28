6fc635608c077f78d031146e9e3fba16638b57c2
Myagmarsuren <miner_miga@yahoo.com>
2021-01-28 22:27:39 +0800

    remove-iig uusgev

--- F.CS311-Lab1/src/test/ArrayQueue.java
+++ F.CS311-Lab1/src/test/ArrayQueue.java
@@ -1,10 +1,24 @@
 package test;
 
-public class ArrayQueue {
+public class ArrayQueue implements Queue {
 
+	int front;         
+	int rear;           		
+	Object [] queue;
+	
+	public Object remove()
+	   {
+	      if (isEmpty())
+	         return null;
+	      front = (front + 1) % queue.length;
+	      Object frontElement = queue[front];
+	      queue[front] = null; 
+	      return frontElement;
+	   }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 
 	}
 
 }
