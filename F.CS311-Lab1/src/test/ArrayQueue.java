7328c72520fb30745a569bc76dcfa102aadd9eec
Ariunsanaa <ariunsanaa.9161@gmail.com>
2021-01-28 22:30:33 -0800

    constructor nemlee

--- F.CS311-Lab1/src/test/ArrayQueue.java
+++ F.CS311-Lab1/src/test/ArrayQueue.java
@@ -6,6 +6,20 @@ public class ArrayQueue implements Queue {
 	int rear;           		
 	Object [] queue;
 	
+	public ArrayQueue(int initialCapacity)
+	{
+      if (initialCapacity < 1)
+	      throw new IllegalArgumentException
+	           ("initialCapacity must be >= 1");
+      queue = new Object [initialCapacity + 1];
+	      // default front = rear = 0
+	}
+	
+	public ArrayQueue()
+	{// use default capacity of 10
+		this(10);
+	}
+	
 	public Object remove()
 	   {
 	      if (isEmpty())
